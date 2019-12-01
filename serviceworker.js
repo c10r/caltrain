cdb0712e9156b909cb3439da9a66c2315e3e44c8
Chander Ramesh <hardly.hacking@gmail.com>
2019-11-30 16:02:38 -0800

    Defer to network, if available - and only then to cache

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,4 @@
-const cacheName = 'hello-pwa'
+const CACHE_NAME = 'caltrain-cache'
 const filesToCache = [
   '/',
   '/index.html',
@@ -9,17 +9,23 @@ const filesToCache = [
 /* Start the service worker and cache all of the app's content */
 self.addEventListener('install', function(e) {
   e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
+    caches.open(CACHE_NAME).then(function(cache) {
       return cache.addAll(filesToCache)
     })
   )
 })
 
 /* Serve cached content when offline */
-self.addEventListener('fetch', function(e) {
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
+    caches
+      .open(CACHE_NAME)
+      .then(cache => {
+        return fetch(e.request)
+          .then(response => {
+            cache.put(e.request, response.clone())
+            return response
+          })
+      })
   )
 })
