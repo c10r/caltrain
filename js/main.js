d755f04a2013be4edbabbd37d27c703c506fe563
Chander Ramesh <hardly.hacking@gmail.com>
2019-12-01 11:05:29 -0800

    Separate the weekend schedules into two separate pages

--- js/main.js
+++ js/main.js
@@ -3,61 +3,85 @@ window.onload = () => {
 
   document.getElementById('legend').style.display = 'none'
   document.getElementById('southbound').style.display = 'none'
-  document.getElementById('weekend').style.display = 'none'
+  document.getElementById('northbound-weekend').style.display = 'none'
+  document.getElementById('southbound-weekend').style.display = 'none'
 
   const legendNav = document.getElementById('legend-nav')
   const northboundNav = document.getElementById('northbound-nav')
   const southboundNav = document.getElementById('southbound-nav')
-  const weekendNav = document.getElementById('weekend-nav')
+  const northboundWeekendNav = document.getElementById('northbound-weekend-nav')
+  const southboundWeekendNav = document.getElementById('southbound-weekend-nav')
 
   const legend = document.getElementById('legend')
   const northbound = document.getElementById('northbound')
   const southbound = document.getElementById('southbound')
-  const weekend = document.getElementById('weekend')
+  const northboundWeekend = document.getElementById('northbound-weekend')
+  const southboundWeekend = document.getElementById('southbound-weekend')
 
   legendNav.addEventListener('click', function() {
     legendNav.classList.add('active')
     northboundNav.classList.remove('active')
     southboundNav.classList.remove('active')
-    weekendNav.classList.remove('active')
+    northboundWeekendNav.classList.remove('active')
+    southboundWeekendNav.classList.remove('active')
 
     legend.style.display = ''
     northbound.style.display = 'none'
     southbound.style.display = 'none'
-    weekend.style.display = 'none'
+    northboundWeekend.style.display = 'none'
+    southboundWeekend.style.display = 'none'
   })
   northboundNav.addEventListener('click', function() {
     legendNav.classList.remove('active')
     northboundNav.classList.add('active')
     southboundNav.classList.remove('active')
-    weekendNav.classList.remove('active')
+    northboundWeekendNav.classList.remove('active')
+    southboundWeekendNav.classList.remove('active')
 
     legend.style.display = 'none'
     northbound.style.display = ''
     southbound.style.display = 'none'
-    weekend.style.display = 'none'
+    northboundWeekend.style.display = 'none'
+    southboundWeekend.style.display = 'none'
   })
   southboundNav.addEventListener('click', function() {
     legendNav.classList.remove('active')
     northboundNav.classList.remove('active')
     southboundNav.classList.add('active')
-    weekendNav.classList.remove('active')
+    northboundWeekendNav.classList.remove('active')
+    southboundWeekendNav.classList.remove('active')
 
     legend.style.display = 'none'
     northbound.style.display = 'none'
     southbound.style.display = ''
-    weekend.style.display = 'none'
+    northboundWeekend.style.display = 'none'
+    southboundWeekend.style.display = 'none'
   })
-  weekendNav.addEventListener('click', function() {
+  northboundWeekendNav.addEventListener('click', function() {
     legendNav.classList.remove('active')
     northboundNav.classList.remove('active')
     southboundNav.classList.remove('active')
-    weekendNav.classList.add('active')
+    northboundWeekendNav.classList.add('active')
+    southboundWeekendNav.classList.remove('active')
 
     legend.style.display = 'none'
     northbound.style.display = 'none'
     southbound.style.display = 'none'
-    weekend.style.display = ''
+    northboundWeekend.style.display = ''
+    southboundWeekend.style.display = 'none'
+  })
+  southboundWeekendNav.addEventListener('click', function() {
+    legendNav.classList.remove('active')
+    northboundNav.classList.remove('active')
+    southboundNav.classList.remove('active')
+    northboundWeekendNav.classList.remove('active')
+    southboundWeekendNav.classList.add('active')
+
+    legend.style.display = 'none'
+    northbound.style.display = 'none'
+    southbound.style.display = 'none'
+    northboundWeekend.style.display = 'none'
+    southboundWeekend.style.display = ''
   })
 
   if ('serviceWorker' in navigator) {
