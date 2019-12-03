37d441e2ea5721bc09bdcfef5b90243e95c20c0a
Chander Ramesh <hardly.hacking@gmail.com>
2019-12-02 18:17:48 -0800

    Change navigation to replace current tab

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+document.getElementById('northbound-nav').addEventListener('click', function(event) {
+  event.preventDefault()
+  window.location.replace('/')
+})
+
+document.getElementById('southbound-nav').addEventListener('click', function(event) {
+  event.preventDefault()
+  window.location.replace('south.html')
+})
+
+document.getElementById('northbound-weekend-nav').addEventListener('click', function(event) {
+  event.preventDefault()
+  window.location.replace('northweekend.html')
+})
+
+document.getElementById('southbound-weekend-nav').addEventListener('click', function(event) {
+  event.preventDefault()
+  window.location.replace('southweekend.html')
+})
+
+document.getElementById('legend-nav').addEventListener('click', function(event) {
+  event.preventDefault()
+  window.location.replace('legend.html')
+})
+
 window.onload = () => {
   'use strict'
 
